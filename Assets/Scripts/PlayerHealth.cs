866af0ae6d2afe77879920a4834c5c33bc6c8a07
takeshifz12345 <fzt4003682@est.univalle.edu>
2025-10-11 18:03:17 -0400

    PlayerHealt.cs
    Se programó todo el script PlayerHealth.cs uwu.

--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -11,36 +11,55 @@ public class PlayerHealth : MonoBehaviour
 
     public void TakeDamage(int damage)
     {
-
+        currentHealth -= damage;
+        currentHealth = Mathf.Clamp(currentHealth, 0, maxHealth);
+        OnHealthChanged();
     }
 
     public void Heal()
     {
+        if (canHeal)
+        {
+            currentHealth += signaLevel;
+            currentHealth = Mathf.Clamp(currentHealth, 0, maxHealth);
+            OnHealthChanged();
 
+            StartHealCooldown();
+        }
     }
 
     public void StartHealCooldown()
     {
-
+        canHeal = false;
+        healCooldownTimer = 0f;
     }
 
     public void UpdateCooldown()
     {
+        if (!canHeal)
+        {
+            healCooldownTimer += Time.deltaTime;
 
+            if (healCooldownTimer >= healCooldown)
+            {
+                canHeal = true;
+                healCooldownTimer = 0f;
+            }
+        }
     }
 
     public void OnHealthChanged()
     {
-
+        //Pa´ luego.
     }
 
     void Start()
     {
-        
+        currentHealth = maxHealth;
     }
 
     void Update()
     {
-        
+        UpdateCooldown();
     }
 }
