08883586556aea686d863f1906b4e65f99ab904e
takeshifz12345 <fzt4003682@est.univalle.edu>
2025-10-11 15:19:29 -0400

    PauseMenu.cs
    Se programó en PauseMenu.cs.

--- Assets/Scripts/PauseMenu.cs
+++ Assets/Scripts/PauseMenu.cs
@@ -4,11 +4,13 @@ public class PauseMenu : MonoBehaviour
 {
     public void Pause()
     {
-
+        Time.timeScale = 0f;
+        Debug.Log("Juego pausado");
     }
 
     public void Resume()
     {
-
+        Time.timeScale = 1f;
+        Debug.Log("Juego reanudado");
     }
 }
