75d6b9a9b184c4b7b1917202e820582cce19272e
takeshifz12345 <fzt4003682@est.univalle.edu>
2025-10-28 09:45:38 -0400

    Cambios uwu

--- Assets/Scripts/Triggers/Level1Stages/Lvl1_Stage2_1.cs
+++ Assets/Scripts/Triggers/Level1Stages/Lvl1_Stage2_1.cs
@@ -8,20 +8,22 @@ public class Level1Stage2_1 : StageController
 
     private void OnTriggerEnter2D(Collider2D other)
     {
-        TriggerStage(other);
-
-        string[] dialog = new string[]
+        if (other.gameObject.CompareTag("Player"))
         {
+            TriggerStage(other);
+
+            string[] dialog = new string[]
+            {
             "—Por si acaso, no pude quitar el desastre que tú y tu amiga hicieron en la pared.",
             "—Jester se quedó sin pintura amarilla.",
             "—No salgas de esta habitación.",
             "—Te estaré viendo."
-        };
+            };
 
-        float[] tiempos = new float[] { 5f, 2f, 2f, 3f };
+            float[] tiempos = new float[] { 5f, 2f, 2f, 3f };
 
-        AttackData[] attacks = new AttackData[]
-        {
+            AttackData[] attacks = new AttackData[]
+            {
             new AttackData
             {
                 prefab = ataque,
@@ -32,9 +34,10 @@ public class Level1Stage2_1 : StageController
                 lifeTime = 3f,
                 delay = 0.5f
             }
-        };
+            };
 
-        // Ejecuta diálogo y luego ataque
-        StartDialog(dialog, tiempos, () => LaunchAttacks(attacks));
+            // Ejecuta diálogo y luego ataque
+            StartDialog(dialog, tiempos, () => LaunchAttacks(attacks));
+        }
     }
 }
--- Assets/Scripts/Triggers/Level1Stages/Lvl1_Stage2_2.cs
+++ Assets/Scripts/Triggers/Level1Stages/Lvl1_Stage2_2.cs
@@ -4,18 +4,21 @@ public class Level1Stage2_2 : StageController
 {
     private void OnTriggerEnter2D(Collider2D other)
     {
-        TriggerStage(other);
-
-        string[] dialog = new string[]
+        if (other.gameObject.CompareTag("Player"))
         {
+            TriggerStage(other);
+
+            string[] dialog = new string[]
+            {
             "PARA SALTAR PULSE ESPACIO O LA TECLA W.",
             "PARA CURARSE PRESIONA LA TECLA Q.",
             "LA VIDA CURADA DEPENDE DE LA SEÑAL QUE SE TENGA.",
             "—Te estaré viendo."
-        };
+            };
 
-        float[] tiempos = new float[] { 5f, 2f, 2f, 3f };
+            float[] tiempos = new float[] { 5f, 2f, 2f, 3f };
 
-        StartDialog(dialog, tiempos);
+            StartDialog(dialog, tiempos);
+        }
     }
 }
