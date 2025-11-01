5bbbfea009b052a588490dc9285c38f5f49c49b6
AngeMate <168114727+AngeMate@users.noreply.github.com>
2025-11-01 17:16:44 -0400

    avance level2

--- Assets/Scripts/Triggers/Level2Stages/Lvl2_Stage4.cs
+++ Assets/Scripts/Triggers/Level2Stages/Lvl2_Stage4.cs
@@ -1,16 +1,90 @@
+using System.Collections;
 using UnityEngine;
 
-public class Lvl2_Stage4 : MonoBehaviour
+public class Lvl2_Stage4 : StageController
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [Header("Configuraci�n general")]
+    public GameObject ataques;
+    public float spawnXA;
+    public float spawnXB;
+    public float spawnY;
+    public float intervaloAtaques = 1f;
+
+    [Header("Zonas y ojos")]
+    public SignalZone zona1, zona2, zona3;
+    public EyeManager ojo1, ojo2, ojo3;
+
+    [Header("Referencias externas")]
+    public GameObject enemigo;
+    public GameObject canvas;
+
+    private Coroutine ataqueLoop;
+
+    private void OnTriggerEnter2D(Collider2D other)
     {
-        
+        if (!TriggerStage(other)) return;
+
+        string[] lines = {
+            "uwu"
+        };
+
+        float[] times = { 3f, 2f, 3f, 4f, 5f };
+
+        StartDialog(lines, times, () =>
+        {
+            canvas.SetActive(true);
+            enemigo.GetComponent<EnemyStatus>().Active();
+            ataqueLoop = StartCoroutine(LanzarAtaquesInfinitos());
+        });
     }
 
-    // Update is called once per frame
-    void Update()
+    private IEnumerator LanzarAtaquesInfinitos()
     {
-        
+        while (enemigo != null)
+        {
+            // 5 rondas de 3 ataques
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    float spawnX = Random.Range(spawnXA, spawnXB);
+                    Vector2 spawnPos = new(spawnX, spawnY);
+                    Instantiate(ataques, spawnPos, Quaternion.identity);
+                }
+                yield return new WaitForSeconds(intervaloAtaques);
+            }
+
+            CambiarEstadosZonas();
+            yield return new WaitForSeconds(2f);
+        }
+
+        TerminarStage();
+    }
+
+    private void CambiarEstadosZonas()
+    {
+        EyeManager[] ojos = { ojo1, ojo2, ojo3};
+        SignalZone[] zonas = { zona1, zona2, zona3};
+
+        for (int i = 0; i < ojos.Length; i++)
+        {
+            if (Random.value > 0.5f)
+            {
+                ojos[i].ChangeState();
+                zonas[i].ChangeState();
+            }
+        }
+    }
+
+    public void TerminarStage()
+    {
+        if (ataqueLoop != null)
+            StopCoroutine(ataqueLoop);
+
+        ojo1.SetOpen(false);
+        ojo2.SetOpen(false);
+        ojo3.SetOpen(false);
+
+        zona1.Enable();
     }
 }
